91ce479f0762a31adb20088cbdab011b7f3d7e46
Adesdesk <aadelakun28@gmail.com>
2023-02-25 22:16:15 +0100

    Update index.js

--- saverse-client/pages/index.js
+++ saverse-client/pages/index.js
@@ -20,12 +20,21 @@ export default function Home() {
       <div>
         <Navbar />
       </div>
-        <div class="bg-gradient-to-r from-slate-500 to-gray-200 h-screen">
-          <p>
-            Get started with 
-            <code className={styles.code}> Saverse thrift administrator</code>
+      <div className="grid grid-cols-6 gap-1 bg-gradient-to-r from-slate-500 to-gray-200 h-screen">      
+      {/*<div className="grid grid-cols-1 gap-12 lg:grid-cols-12 bg-gradient-to-r from-slate-500 to-gray-200 h-screen">*/}
+      <div className="col-start-1 col-end-6 rounded-r-lg bg-gradient-to-r from-white to-gray-200 mx-2 my-2">
+     
+        {/*<div className="col-span-1 rounded-r-lg bg-gradient-to-r from-white to-gray-200 mx-2 my-2 lg:col-span-9">*/}
+          <p className="ml-2 ext-white flex space-x-4">
+            Get started with Saverse thrift administrator
           </p>
         </div>
+        
+        <div className="col-end-7 col-span-1 rounded-l-lg bg-gradient-to-r from-black to-gray-200 mx-2 my-2">
+          
+          </div>
+          </div>
+        
         <Footer />
     </>
   )
